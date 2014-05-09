8cd28f96cf50aeac79d21422e5801f0284b3a181
tzuhan <stoola08@gmail.com>
2014-05-10 00:24:02 +0800

    upload all directories

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CcspFinal::Application.config.secret_token = '742c6d0a92866aa93bf552fca710eb14e5d26b54b051a278b08c0892df982c0f6b867b8c9ddee709dc3f59b69d08f971ed5ec79716c16b38b9f20148d15f3da0'
