9bb900b98678b29e4dcbd561dfd134616fa8a059
Eugene Hsu <dreammacer.yo@gmail.com>
2014-06-05 02:15:06 +0800

    一大堆CSS by Yo

--- app/assets/javascripts/index_welcome.js
+++ app/assets/javascripts/index_welcome.js
@@ -1,19 +1,19 @@
 (function() {
     $(document).ready(function() {
         // tzuhan
-        $("a").click(function(){
+        $("a").click(function() {
             $(this).addClass('.clicked');
             console.log("add clicked");
         });
-        $("div.clicked").click(function(){
+        $("div.clicked").click(function() {
             $(this).removeClass('.clicked');
         });
 
         // Yo
         var foldingList = $('.folding'),
-        foldingListHeight = $('.folding').height(),
-        topElemOffset = foldingList.offset().top;
-            // Function responsible for unfolding the list
+            foldingListHeight = $('.folding').height(),
+            topElemOffset = foldingList.offset().top;
+        // Function responsible for unfolding the list
         unfold = function() {
             setTimeout(function() {
                 if (!foldingList.hasClass('folded')) {
@@ -56,7 +56,7 @@
         // Check whether to unfold the list when scrolling/resizing
         $(window).on("scroll resize", function() {
             var th = $(this);
-            if (th.scrollTop() + th.height() - foldingListHeight >= 3*topElemOffset)
+            if (th.scrollTop() + th.height() - foldingListHeight >= 3 * topElemOffset)
                 unfold();
         })
 
@@ -70,17 +70,72 @@
             robotDiv.animate({
                 'height': '300px',
             });
-        }
+        };
+
+
+
+        //Yo
+        $('.answer').on('click', function() {
+            $(this).toggleClass('active btn-warning btn-default');
+        });
+
+        var toduration = 2000;
+        $('.qt1').find('.answer').on('click', function() {
+            if ($(this).hasClass('correct')) {
+                var notify = humane.create({
+                    timeout: toduration,
+                    baseCls: 'humane-bigbox'
+                })
+                notify.log('（V）<br>太棒了！請繼續往下一題前進～');
+                $('.humane-bigbox').css({
+                    "background": "rgba(84, 173, 71, 0.93)"
+                });
+                setTimeout(function() {
+                    $('.qt1').addClass('folded');
+                    $('.qt1').find('.correct').removeClass('btn-warning').addClass('btn-default');
+                }, toduration + 500);
+            } else {
+                var notify = humane.create({
+                    timeout: 2000,
+                    baseCls: 'humane-bigbox'
+                })
+                notify.log('（X）<br>答錯了，看清楚再好好作答哦～');
+                $('.humane-bigbox').css({
+                    "background": "rgba(196, 57, 57, 0.93)"
+                });
+                setTimeout(function() {
+                    $('.qt1').find('.wrong').removeClass('btn-warning').addClass('btn-default');
+                }, toduration);
+
+            }
+
+
+
+        })
+
+
+
+
+
+
+
+
     });
+
+
+
+
+
+
+
+
 })();
 
 $(document).ready(function() {
-  $('.tinmancontainer').css('right',-$('.tinmancontainer').outerWidth());
-  $('#tinman').click(function() {
-    $('.tinmancontainer').animate({
-      right: parseInt($('.tinmancontainer').css('right'),10) == 0 ?
-        -$('.tinmancontainer').outerWidth() :
-        0
+    $('.tinmancontainer').css('left', -$('.tinmancontainer').outerWidth());
+    $('#tinman').click(function() {
+        $('.tinmancontainer').animate({
+            left: parseInt($('.tinmancontainer').css('left'), 10) == 0 ? -$('.tinmancontainer').outerWidth() : 0
+        });
     });
-  });
 });
